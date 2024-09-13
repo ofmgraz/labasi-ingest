cbd3f185f980a3e4b7a8e58279aa3e7da0f57811
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-13 08:59:34 +0200

    only md ingest for starters

--- fetch_data.sh
+++ fetch_data.sh
@@ -9,18 +9,18 @@ echo "downloding arche.tll"
 wget https://labasi.acdh.oeaw.ac.at/to-ingest/arche.ttl -O to_ingest/arche.ttl
 echo "################"
 
-echo "################"
-echo "downloding and unzipping sign images"
-wget https://labasi.acdh.oeaw.ac.at/to-ingest/signs.zip -O to_ingest/zip.zip
-unzip to_ingest/zip.zip -d to_ingest
-rm to_ingest/zip.zip
-echo "################"
+# echo "################"
+# echo "downloding and unzipping sign images"
+# wget https://labasi.acdh.oeaw.ac.at/to-ingest/signs.zip -O to_ingest/zip.zip
+# unzip to_ingest/zip.zip -d to_ingest
+# rm to_ingest/zip.zip
+# echo "################"
 
-echo "################"
-echo "downloding and unzipping glyph zips"
-wget https://labasi.acdh.oeaw.ac.at/to-ingest/glyphs.zip -O to_ingest/zip.zip
-unzip to_ingest/zip.zip -d to_ingest
-rm to_ingest/zip.zip
-echo "##############"
+# echo "################"
+# echo "downloding and unzipping glyph zips"
+# wget https://labasi.acdh.oeaw.ac.at/to-ingest/glyphs.zip -O to_ingest/zip.zip
+# unzip to_ingest/zip.zip -d to_ingest
+# rm to_ingest/zip.zip
+# echo "##############"
 
-wget "https://labasi.acdh.oeaw.ac.at/static/webpage/img/labasi_logo.jpg" -O to_ingest/labasi_logo.jpg
+# wget "https://labasi.acdh.oeaw.ac.at/static/webpage/img/labasi_logo.jpg" -O to_ingest/labasi_logo.jpg
