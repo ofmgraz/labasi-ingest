2466d41cdc7962483b57f8cd052b5b3e548c3907
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-13 08:52:26 +0200

    init commit

---
+++ arche__filechecker.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+mv ./to_ingest/arche.ttl ./to_ingest/arche.txt
+echo "run filechecker for for ${TOPCOLID}"
+rm -rf ${PWD}/fc_out && mkdir ${PWD}/fc_out
+docker run \
+  --rm \
+  --network="host" \
+  -v ${PWD}/fc_out:/reports \
+  -v ${PWD}/to_ingest:/data \
+  --entrypoint arche-filechecker \
+  acdhch/arche-ingest \
+  --overwrite --skipWarnings /data /reports
+mv ./to_ingest/arche.txt ./to_ingest/arche.ttl
